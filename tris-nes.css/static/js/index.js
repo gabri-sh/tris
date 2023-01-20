99d7933b2981fbd6ed669d503b91f49aee73c2d0
Gabriele Di Cosmo <gabrieledicosmo05@gmail.com>
2023-01-20 17:34:04 +0100

    aggiunte funzioni js
    funzioni chekplayer, cambiato il currentPlayer ad ogni turno, aggiunto refresh della pagina alla pressione del button

--- tris-nes.css/static/js/index.js
+++ tris-nes.css/static/js/index.js
@@ -1,5 +1,6 @@
 //il controllo della vittoria, verrà fatto in base al player che cambierà ogni turno
 let cells = document.querySelectorAll(".button");
+let resetBtn = document.querySelector(".resetBtn");
 
 function checkWinner(player) {
 	if (grid[0][0] === player && grid[1][1] === player && grid[2][2] === player) {
@@ -51,6 +52,19 @@ function checkWinner(player) {
 	}
 }
 
+function RefreshWindow() {
+	window.location.reload(true);
+}
+
+resetBtn.addEventListener("click", function (e) {
+	e.preventDefault();
+	RefreshWindow();
+});
+
+//il reset button e la funzione che verifica il vincitore funzionano
+//resta da implementare la parte che quando spingi una casella esce l'icona del personaggio su quella casella
+//per decrertare il vincitore, fare div con all'interno uno span di output che dirà il giocatore vincitore
+
 let currentPlayer = "X";
 let grid = [
 	[null, null, null],
@@ -62,78 +76,32 @@ for (let i = 0; i < cells.length; i++) {
 	cells[i].addEventListener("click", function (e) {
 		e.preventDefault();
 
+		console.log(`valore della i --> ${i}`);
+		console.log(`valore di player --> ${currentPlayer}`);
+		
+		if (currentPlayer === "X") {
+			cells[i].innerHTML = "X"
+		} else {
+			cells[i].innerHTML = "O"
+		}
+				
+		let win = checkWinner(currentPlayer)
+		
+		//punto 6
+		if (currentPlayer === "X") {
+			currentPlayer = "O"
+		}else{
+			currentPlayer = "X"
+		}
 	});
 }
 
+
 /*
-let array = [] //in Javascript non serve dichiarare la dimensione all'inizio 
-
-array.push(1) //push aggiunge un elemento nell'array
-array.push(2)
-array.push(3)
-array.push(4)
-array.push(5)
-
-console.log(array[0])
-
-let lastEl = array.pop() //pop rimuove l'ultimo elemento
-console.log(array) // 1, 2, 3, 4
-
-for(let i = 0; i<array.length; i++){
-    console.log(array[i])
-}
-
-//array bidimensionali: array = elenco di valori, array bidimensionale: ogni elemento è un array
-
-let grid = [
-    ["X", "O", "X"],
-    ["O", null, "X"],
-    [null, null, null], //il giocatore dovrebbe mettere il cerchio in basso a destra, quindi:
-]
-
-grid[2][2] = "O"
-
-console.log(grid)
-
-for(let i = 0; i < grid.length; i++){
-    let row = grid[i]
-    for(let j = 0; i < row.length; i++){
-        console.log(`Posizione ${i}, ${j} -> ${row[j]}`)
-    }
-}
-
-let player
-
-function checkMainDiag(player){ //player può essere o "X" o "O"
-    if(grid[0][0] === player && grid[1][1] === player && grid[2][2] === player){
-        return true
-    }else{
-        return false
-    }
-}
-
-checkMainDiag(player)
-
-function checkWinner(player){
-    if(fare il controllo della diagonale 1){
-        return true;
-    }else if(controllo della diagonale 2){
-        return true;
-    }else if(controllo delle altre 3 rige e poi fare lo stesso per le colonne){
-        conbinazioni quelle sia in verticale che in orizzontale
-        return true;
-    }else{
-        return false
-    }
-}
-
-
-
 1. Inizializzo grigia di gioco vuota (tutti elementi = null)
 2. Creo una variabile currentPlayer
 3. Event listener sulle celle della griglia - per associare le celle all'elemento della grid in js usare data attribute oppure id=1,2,3...
 4. Disegnare X o O nella cella cliccata solo se non è già occupata 
 5. Controllare dopo ogni mossa se il currentPlayer ha vinto 
 6. Cambiare il currentPlayer
-
 */
